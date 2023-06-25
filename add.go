321a7660d8cdbdcf3e7e289e2b539002db9803a0
Thareq Arsyad Darmawan <=>
2023-06-25 19:43:54 +0700

    add module

--- add.go
+++ add.go
@@ -1,8 +1,8 @@
 package moduler
 
-func Add(numbers []int64) (result int64) {
-	for i := 0; i < len(numbers); i++ {
-		result += numbers[i]
+func Add(numbers ...int64) (result int64) {
+	for _, v := range numbers {
+		result += v
 	}
 	return
 }
