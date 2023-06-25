07df76986a46b4369d6699a8923686158f9369e9
Thareq Arsyad Darmawan <=>
2023-06-25 23:49:17 +0700

    update subs && abs

---
+++ abs.go
@@ -0,0 +1,5 @@
+package moduler
+
+func Abs(numberOne int64, numberTwo int64) float64 {
+	return float64(numberOne) * float64(numberTwo)
+}
---
+++ substract.go
@@ -0,0 +1,6 @@
+package moduler
+
+func Substract(numberOne int64, numberTwo int64) (result int64) {
+	result = numberOne - numberTwo
+	return
+}
